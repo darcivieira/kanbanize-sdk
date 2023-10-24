2702b6e46c61dabc315c9c820d36cc5b258b20ca
darcivieira <darcivieira.junior@gmail.com>
2023-10-23 22:35:24 -0300

    Initial Users Model

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,3 +7,17 @@ def test_list_users(requests_mock):
     test_json = [{'user_id': 1, 'email': 'teste@teste.com'}]
     requests_mock.get('https://teste.kanbanize.com/api/v2/users', json=test_json)
     assert service.users().list().json() == test_json
+
+
+def test_get_user(requests_mock):
+    service = kanbanize({'subdomain': 'teste', 'api_key': 'teste_key'})
+    test_json = [{'user_id': 1, 'email': 'teste@teste.com'}]
+    requests_mock.get('https://teste.kanbanize.com/api/v2/users', json=test_json)
+    assert service.users().list().json() == test_json
+
+
+def test_invite_user(requests_mock):
+    service = kanbanize({'subdomain': 'teste', 'api_key': 'teste_key'})
+    test_json = [{'user_id': 1, 'email': 'teste@teste.com'}]
+    requests_mock.get('https://teste.kanbanize.com/api/v2/users', json=test_json)
+    assert service.users().list().json() == test_json
